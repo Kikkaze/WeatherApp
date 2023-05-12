5eca08b4bd13dc5fb8c5a2e38f8b5a597ff74bb3
Kikkaze <132634188+Kikkaze@users.noreply.github.com>
2023-05-12 00:09:36 -0700

    Added js to show weekday

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function showWeekday(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}`;
+}
+
 function showTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
@@ -11,9 +26,17 @@ function showTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let feelsLikeElement = document.querySelector("#feelsLike");
   feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  let dayElement = document.querySelector("#weekday");
+  dayElement.innerHTML = showWeekday(response.data.time * 1000);
+  let iconElement = document.querySelector("#mainIcon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
 }
 
 let apiKey = "368a5t8b9e10a64e78c55aob0f467b4c";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=San Francisco&key=${apiKey}`;
+let city = "San Francisco";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showTemperature);
