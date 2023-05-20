45a2e5b3126e9b99f3a7863d1efc27790b2ca9fc
Kikkaze <132634188+Kikkaze@users.noreply.github.com>
2023-05-20 15:50:13 -0700

    Added forecast, styled it and moved to js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 function showWeekday(timestamp) {
   let date = new Date(timestamp);
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let dayMonth = date.getDate();
+  let year = date.getFullYear();
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = `${month} ${dayMonth}, ${year}`;
+
   let days = [
     "Sunday",
     "Monday",
@@ -10,7 +30,43 @@ function showWeekday(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+  let weekday = document.querySelector("#weekday");
+  weekday.innerHTML = `${day}`;
   return `${day}`;
+}
+
+function showForecast() {
+  let forecastElement = document.querySelector("#following-days");
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2 forecast">
+              <span id="forecast-day">${day}</span>
+              <br />
+              <img
+                id="forecast-icon"
+                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
+                alt="sun and clouds icon"
+              />
+              <br />
+              <span id="forecast-temp-max">20</span>° /
+              <span id="forecast-temp-min">15</span>°
+            </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "368a5t8b9e10a64e78c55aob0f467b4c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function showTemperature(response) {
@@ -34,6 +90,8 @@ function showTemperature(response) {
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.description);
+
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
@@ -53,3 +111,5 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchWeather);
 
 search("San Francisco");
+
+showForecast();
